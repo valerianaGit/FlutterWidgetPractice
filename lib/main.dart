804598881ca1b59743c905027747359a7abed71d
Valeriana <valeria.do@gmail.com>
2019-04-10 10:54:23 -0500

    Sizing widgets inside a row or column

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,51 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-  return Container(
-decoration: BoxDecoration( color: Colors.white),
-child: Center(
-  child: Text("Hello World", textDirection: TextDirection.ltr, style: TextStyle(fontSize: 32, color: Colors.black87,),),
-),
-  );
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          "Hello World",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    );
   }
 }
+
+Widget buildRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Expanded(
+        flex: 2,
+     child: Image.asset("1.png"),
+      ),
+      Expanded(
+     child: Icon(Icons.star),
+      ),
+      Expanded(
+     child: Icon(Icons.subway),
+      ),
+    ],
+  );
+}
+
+Widget buildColumn() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Image.asset("1.png"),
+      Icon(Icons.cake),
+      Icon(Icons.email),
+    ],
+  );
+}
+
 /*
 class MyHomePage extends StatefulWidget {
   @override

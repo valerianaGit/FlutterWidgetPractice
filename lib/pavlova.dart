98fcbb19a5b61f786f84c94486c508319243e5d1
Valeriana <valeria.do@gmail.com>
2019-04-10 16:12:00 -0500

    Build custom book cell

--- lib/pavlova.dart
+++ lib/pavlova.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,125 +6,123 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
- return MaterialApp(
-   title: 'Flutter Layout practice',
-   home: iconList(),
- );
-  } }
+    return MaterialApp(
+      title: 'Flutter Layout practice',
+      home: iconList(),
+    );
+  }
 
+  Widget buildRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min, //packs children closely together
+      children: [
+        //building the array of children for the Row
+        Expanded(
+          flex: 2, //taking up twice as much space as the other siblings
+          child: Image.asset("1.png"),
+        ),
+        Expanded(
+          child: Icon(Icons.star),
+        ),
+        Expanded(
+          child: Icon(Icons.subway),
+        ),
+      ],
+    );
+  }
 
-Widget buildRow() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    mainAxisSize: MainAxisSize.min, //packs children closely together
-    children: [
-      //building the array of children for the Row
-      Expanded(
-        flex: 2, //taking up twice as much space as the other siblings
-        child: Image.asset("1.png"),
+  Widget buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset("1.png"),
+        Icon(Icons.cake),
+        Icon(Icons.email),
+      ],
+    );
+  }
+
+  Widget iconList() {
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w900,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Icon(Icons.kitchen, color: Colors.green[300]),
+                Text('Prep:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.timer, color: Colors.green),
+                Text('Cook:'),
+                Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(Icons.restaurant, color: Colors.green),
+                Text('Feeds:'),
+                Text('4-6'),
+              ],
+            ),
+          ],
+        ),
       ),
-      Expanded(
-        child: Icon(Icons.star),
-      ),
-      Expanded(
-        child: Icon(Icons.subway),
-      ),
-    ],
-  );
-}
-
-Widget buildColumn() {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      Image.asset("1.png"),
-      Icon(Icons.cake),
-      Icon(Icons.email),
-    ],
-  );
-}
-
-
-
-Widget iconList() {
-  final descTextStyle = TextStyle(
-    color: Colors.black,
-    fontWeight: FontWeight.w900,
-    fontFamily: 'Roboto',
-    letterSpacing: 0.5,
-    fontSize: 18,
-    height: 2,
-  );
-  final iconList = DefaultTextStyle.merge(
-    style: descTextStyle,
-    child: Container(
+    );
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.black),
+        Icon(Icons.star, color: Colors.white),
+      ],
+    );
+    final ratings = Container(
       padding: EdgeInsets.all(16.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Column(
-            children: <Widget>[
-              Icon(Icons.kitchen, color: Colors.green[300]),
-              Text('Prep:'),
-              Text('25 min'),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              Icon(Icons.timer, color: Colors.green),
-              Text('Cook:'),
-              Text('1 hr'),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              Icon(Icons.restaurant, color: Colors.green),
-              Text('Feeds:'),
-              Text('4-6'),
-            ],
+          stars,
+          Text(
+            "170 Reviews",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
           ),
         ],
       ),
-    ),
-  );
-  var stars = Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.black),
-      Icon(Icons.star, color: Colors.white),
-    ],
-  );
-  final ratings = Container(
-    padding: EdgeInsets.all(16.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        stars,
-        Text(
-          "170 Reviews",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w800,
-            fontFamily: 'Roboto',
-            letterSpacing: 0.5,
-            fontSize: 20,
-          ),
-        ),
-      ],
-    ),
-  );
-  final leftColumn = Container(
-    padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-    child: Column(
-      children: <Widget>[
-      //  titleText,
-        //subtitle,
-        ratings,
-        iconList,
-      ],
-    ),
-  );
+    );
+    final leftColumn = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: <Widget>[
+          //  titleText,
+          //subtitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+  }
 }
---
+++ lib/productivity.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+Widget productivityCell() {
+  var book = Container(
+    padding: EdgeInsets.all(8.0),
+    child: Icon(Icons.book, color: Colors.green),
+  );
+  var category = Text(
+    'Category',
+    style: TextStyle(
+      fontFamily: 'Georgia',
+      fontWeight: FontWeight.w500,
+      fontSize: 30.0,
+    ),
+  );
+  var subCategory = Text(
+    'SubCategory',
+    style: TextStyle(
+      fontFamily: 'Georgia',
+      fontWeight: FontWeight.w500,
+      fontSize: 20.0,
+    ),
+  );
+  final titleColumn = Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: <Widget>[category, subCategory],
+  );
+  final cellRow = Row(
+//book + title column
+mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+children: <Widget>[book, titleColumn],
+      );
+  final cellCard = Card(
+      //include cell row in here, so I get the rounded corners from Card
+      );
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('My Organized Life'),
+    ),
+    body: Center(
+      child: Container(
+        margin: EdgeInsets.all(16.0),
+        height: 50,
+        child: Card(
+            child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: cellCard,
+//cellRow with insets of 16  and rounded corners -card
+        )),
+      ),
+    ),
+  );
+}

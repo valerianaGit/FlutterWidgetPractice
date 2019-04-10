bf7a5b61ae0f6df2a91e51c07c797914fd6fc115
Valeriana <valeria.do@gmail.com>
2019-04-10 12:29:48 -0500

    Added pavlova file. Made main.dart file to launch the pavlova section with implementation of stacked -embedded columns and rows

--- lib/main.dart
+++ lib/main.dart
@@ -1,141 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
+void main() {
+  debugPaintSizeEnabled = false; // Set to true for visual layout
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          "Hello World",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
-          ),
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: buildHomePage('Strawberry Pavlova Recipe'),
+    );
+  }
+
+  Widget buildHomePage(String title) {
+    final titleText = Container(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        'Strawberry Pavlova',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          letterSpacing: 0.5,
+          fontSize: 30,
         ),
       ),
     );
-  }
-}
 
-Widget buildRow() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    mainAxisSize: MainAxisSize.min, //packs children closely together
-    children: [
-      //building the array of children for the Row
-      Expanded(
-        flex: 2, //taking up twice as much space as the other siblings
-        child: Image.asset("1.png"),
+    final subTitle = Text(
+      'Pavlova is a meringue-based dessert named after the Russian ballerina '
+          'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
+          'topped with fruit and whipped cream.',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontFamily: 'Georgia',
+        fontSize: 25,
       ),
-      Expanded(
-        child: Icon(Icons.star),
-      ),
-      Expanded(
-        child: Icon(Icons.subway),
-      ),
-    ],
-  );
-}
+    );
 
-Widget buildColumn() {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      Image.asset("1.png"),
-      Icon(Icons.cake),
-      Icon(Icons.email),
-    ],
-  );
-}
+    // #docregion ratings, stars
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.black),
+        Icon(Icons.star, color: Colors.black),
+      ],
+    );
+    // #enddocregion stars
 
-
-
-Widget iconList() {
-  final descTextStyle = TextStyle(
-    color: Colors.black,
-    fontWeight: FontWeight.w900,
-    fontFamily: 'Roboto',
-    letterSpacing: 0.5,
-    fontSize: 18,
-    height: 2,
-  );
-  final iconList = DefaultTextStyle.merge(
-    style: descTextStyle,
-    child: Container(
-      padding: EdgeInsets.all(16.0),
+    final ratings = Container(
+      padding: EdgeInsets.all(20),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Icon(Icons.kitchen, color: Colors.green[300]),
-              Text('Prep:'),
-              Text('25 min'),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              Icon(Icons.timer, color: Colors.green),
-              Text('Cook:'),
-              Text('1 hr'),
-            ],
-          ),
-          Column(
-            children: <Widget>[
-              Icon(Icons.restaurant, color: Colors.green),
-              Text('Feeds:'),
-              Text('4-6'),
-            ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          stars,
+          Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
           ),
         ],
       ),
-    ),
-  );
-  var stars = Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.green[500]),
-      Icon(Icons.star, color: Colors.black),
-      Icon(Icons.star, color: Colors.white),
-    ],
-  );
-  final ratings = Container(
-    padding: EdgeInsets.all(16.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        stars,
-        Text(
-          "170 Reviews",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w800,
-            fontFamily: 'Roboto',
-            letterSpacing: 0.5,
-            fontSize: 20,
+    );
+    // #enddocregion ratings
+
+    // #docregion iconList
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    // DefaultTextStyle.merge() allows you to create a default text
+    // style that is inherited by its child and all subsequent children.
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                Text('PREP:'),
+                Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                Text('COOK:'),
+                Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                Text('FEEDS:'),
+                Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+    // #enddocregion iconList
+
+    // #docregion leftColumn
+    final leftColumn = Container(
+      padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          titleText,
+          subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+    // #enddocregion leftColumn
+
+    final mainImage = Image.asset(
+      'images/pavlova.jpg',
+      fit: BoxFit.cover,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      // #docregion body
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(0, 40, 0, 30),
+          height: 600,
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 440,
+                  child: leftColumn,
+                ),
+                mainImage,
+              ],
+            ),
           ),
         ),
-      ],
-    ),
-  );
-
-
-  final leftColumn = Container(
-    padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-    child: Column(
-      children: <Widget>[
-        //titleText,
-        //subtitle,
-        ratings,
-        iconList,
-      ],
-    ),
-  );
+      ),
+      // #enddocregion body
+    );
+  }
 }
